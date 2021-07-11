df4b586d632d6ba6f873717ee91ae275a611af95
LAgrl <lizethariasc14@gmail.com>
2021-07-11 14:55:21 -0700

    finished routes and testing

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = new Sequelize('ecommerce_db', 'root', '', {
+const sequelize = new Sequelize('ecommerce_db', 'root', 'Arias.14', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -71,9 +71,10 @@ router.post('/', (req, res) => {
     }
   */
   Product.create({
-    producct_name: req.body.producct_name,
+    product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
+    category_id: req.body.category_id,
     tagIds: req.body.tagIds
   })
     .then((product) => {
